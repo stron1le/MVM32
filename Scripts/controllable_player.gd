2fbf77030f320ecba1f6dd2dfa85109177f128fc
CaimNidhoggr <57159458+stron1le@users.noreply.github.com>
2026-05-16 11:02:12 -0400

    changed movement to use acceleration instead of cutting speed immediately.

--- Scripts/controllable_player.gd
+++ Scripts/controllable_player.gd
@@ -2,6 +2,8 @@ extends CharacterBody3D
 
 
 const SPEED = 5.0
+const ACCEL = 10.0;
+const BRAKE = 5.0;
 const JUMP_VELOCITY = 4.5
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.01
 @export var tilt_limit = deg_to_rad(75)
@@ -30,12 +32,16 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (_camera_yaw.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
-	if direction:
-		velocity.x = direction.x * SPEED
-		velocity.z = direction.z * SPEED
+	var target_direction = (_camera_yaw.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	if target_direction:
+		var currentMovement:Vector3 = Vector3(velocity.x,0,velocity.z);
+		var targetMovement=target_direction*SPEED;
+		currentMovement=currentMovement.move_toward(targetMovement,ACCEL*delta);
+		velocity.x=currentMovement.x;
+		velocity.z=currentMovement.z;
 	else:
-		velocity.x = move_toward(velocity.x, 0, SPEED)
-		velocity.z = move_toward(velocity.z, 0, SPEED)
-
+		var currentMovement:Vector3 = Vector3(velocity.x,0,velocity.z);
+		currentMovement=currentMovement.move_toward(Vector3.ZERO,BRAKE*delta);
+		velocity.x=currentMovement.x;
+		velocity.z=currentMovement.z;
 	move_and_slide()
