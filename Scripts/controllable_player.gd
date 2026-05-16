ec76665e3b5234adb82c64ddfc45809527bc9b12
CaimNidhoggr <57159458+stron1le@users.noreply.github.com>
2026-05-16 09:19:38 -0400

    Added camera and controls. need to make movement relative

--- Scripts/controllable_player.gd
+++ Scripts/controllable_player.gd
@@ -3,8 +3,19 @@ extends CharacterBody3D
 
 const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
+@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+@export var tilt_limit = deg_to_rad(75)
+@onready var _camera := $CameraPivot/PlayerCam as Camera3D
+@onready var _camera_pivot := $CameraPivot as Node3D
 
-
+func _unhandled_input(event: InputEvent) -> void:
+	if !_camera_pivot:
+		return;
+	if event is InputEventMouseMotion:
+		_camera_pivot.rotation.x -= event.relative.y * mouse_sensitivity
+		# Prevent the camera from rotating too far up or down.
+		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
+		_camera_pivot.rotation.y += -event.relative.x * mouse_sensitivity
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
