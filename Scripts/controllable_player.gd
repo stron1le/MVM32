a6c9256b1a6d847811332db0b53ad7e3bb46eb40
CaimNidhoggr <57159458+stron1le@users.noreply.github.com>
2026-05-16 10:10:14 -0400

    added movement related to camera yaw. Note: we will need to use a different approach for dynamic cameras, because this assumes there is a transform with the camera's yaw. Either that, or we will need to add yaw as a 'metadata' attribute, and make behavior to keep the yaw parallel with the xz plane

--- Scripts/controllable_player.gd
+++ Scripts/controllable_player.gd
@@ -5,17 +5,19 @@ const SPEED = 5.0
 const JUMP_VELOCITY = 4.5
 @export_range(0.0, 1.0) var mouse_sensitivity = 0.01
 @export var tilt_limit = deg_to_rad(75)
-@onready var _camera := $CameraPivot/PlayerCam as Camera3D
-@onready var _camera_pivot := $CameraPivot as Node3D
+@onready var _camera := $CameraYaw/CameraPivot/PlayerCam as Camera3D
+@onready var _camera_pivot := $CameraYaw/CameraPivot as Node3D
+@onready var _camera_yaw :=$CameraYaw as Node3D;
 
 func _unhandled_input(event: InputEvent) -> void:
-	if !_camera_pivot:
+	if !_camera_pivot or !_camera_yaw:
 		return;
 	if event is InputEventMouseMotion:
-		_camera_pivot.rotation.x -= event.relative.y * mouse_sensitivity
+		_camera_yaw.rotation.y -= event.relative.x * mouse_sensitivity
 		# Prevent the camera from rotating too far up or down.
+		_camera_pivot.rotation.x += -event.relative.y * mouse_sensitivity
 		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
-		_camera_pivot.rotation.y += -event.relative.x * mouse_sensitivity
+		
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
@@ -28,7 +30,7 @@ func _physics_process(delta):
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
 	var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-	var direction = (transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+	var direction = (_camera_yaw.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 	if direction:
 		velocity.x = direction.x * SPEED
 		velocity.z = direction.z * SPEED
