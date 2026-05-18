fdd0976c23abed6b7f727f7c91a18bb0c894d297
CaimNidhoggr <57159458+stron1le@users.noreply.github.com>
2026-05-17 21:25:54 -0400

    modified camera to be separated from player. Movement is notw not dependent specifically on the player camera, but upon transform derived based on the current camera. ledge grab now positions player 'below' the upper raycast of the ledge grab, so grabbing behavior is consistent.

--- Scripts/controllable_player.gd
+++ Scripts/controllable_player.gd
@@ -4,43 +4,24 @@ extends CharacterBody3D
 const SPEED = 5.0
 const ACCEL = 10.0;
 const BRAKE = 5.0;
-const JUMP_VELOCITY = 6
-@export_range(0,360) var gamepad_sensitivity = 180;
-@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
-@export var tilt_limit = deg_to_rad(75)
-@onready var _camera := $CameraYaw/CameraPivot/PlayerCam as Camera3D
-@onready var _camera_pivot := $CameraYaw/CameraPivot as Node3D
-@onready var _camera_yaw :=$CameraYaw as Node3D;
-var state="Normal";
-func _unhandled_input(event: InputEvent) -> void:
-	if !_camera_pivot or !_camera_yaw:
-		return;
-	if event is InputEventMouseMotion:
-		_camera_yaw.rotation.y -= event.relative.x * mouse_sensitivity
-		# Prevent the camera from rotating too far up or down.
-		_camera_pivot.rotation.x += -event.relative.y * mouse_sensitivity
-		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
-		
+const JUMP_VELOCITY = 8
+
+var state="Grounded";
 func _physics_process(delta):
 	# Add the gravity.
 	if not is_on_floor():
 		if state!="LedgeGrab":
 			velocity += get_gravity() * delta
-	var cam_dir=Input.get_vector("ui_cam_left","ui_cam_right","ui_cam_up","ui_cam_down");
-	if cam_dir:
-		_camera_yaw.rotation.y -= deg_to_rad(cam_dir.x*gamepad_sensitivity*delta);
-		# Prevent the camera from rotating too far up or down.
-		_camera_pivot.rotation.x += deg_to_rad(cam_dir.y*gamepad_sensitivity*delta);
-		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
 	match(state):
-		"Normal":
+		"Grounded":
 			# Handle jump.
 			if Input.is_action_just_pressed("ui_accept") and is_on_floor():
 				velocity.y = JUMP_VELOCITY
 			# Get the input direction and handle the movement/deceleration.
 			# As good practice, you should replace UI actions with custom gameplay actions.
 			var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-			var target_direction = (_camera_yaw.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+			var moveTransform = get_cam_transform();
+			var target_direction = (moveTransform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 			if target_direction:
 				var currentMovement:Vector3 = Vector3(velocity.x,0,velocity.z);
 				var targetMovement=target_direction*SPEED;
@@ -57,20 +38,19 @@ func _physics_process(delta):
 		"LedgeGrab":
 			if Input.is_action_just_pressed("ui_accept"):
 				velocity.y = JUMP_VELOCITY
-				state="Normal";
+				state="Grounded";
 			else:
 				var input_dir = Input.get_vector("ui_left", "ui_right", "ui_up", "ui_down")
-				var target_direction = (_camera_yaw.transform.basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
+				var target_direction = (get_cam_transform().basis * Vector3(input_dir.x, 0, input_dir.y)).normalized()
 				if (target_direction and target_direction.angle_to(-$EdgeChecker.basis.z)>deg_to_rad(150)):
 					velocity.y=0;
-					state="Normal";
+					state="Grounded";
 					print("Exit");
 func _on_edge_checker_edge_detected(forwardDirection):
 	if !is_on_floor() and velocity.y<0:
 		var savedPosition=global_position;
 		var collisionInfo = move_and_collide(forwardDirection);
 		if (collisionInfo):
-			print(collisionInfo);
 			if (collisionInfo.get_normal().y!=0):
 				global_position=savedPosition;
 				return;
@@ -78,8 +58,22 @@ func _on_edge_checker_edge_detected(forwardDirection):
 		$EdgeChecker/DownCast.force_raycast_update();
 		if ($EdgeChecker/DownCast.is_colliding()):
 			velocity=Vector3.ZERO;
+			global_position.y=$EdgeChecker/DownCast.get_collision_point().y-2;
 			state="LedgeGrab";
 			print("I'm on a ledge")
 		else:
 			global_position=savedPosition;
 	pass # Replace with function body.
+func get_cam_transform():
+	if (get_viewport().get_camera_3d()==Player_Cam.singleton._camera):
+		return Player_Cam.singleton.transform;
+	else:
+		var currentCam = get_viewport().get_camera_3d().transform;
+		var newTransform = Transform3D();
+		var newCamBasis = Basis();
+		newCamBasis.z=currentCam.basis.z;
+		newCamBasis.z.y=0;
+		newCamBasis.x.y=0;
+		newCamBasis=newCamBasis.orthonormalized();
+		newTransform.basis=newCamBasis;
+		return newTransform;
--- Scripts/player_cam.gd
+++ Scripts/player_cam.gd
@@ -1,10 +1,34 @@
-extends Node3D
-
+class_name Player_Cam extends Node3D
+@onready var _camera := $CameraPivot/PlayerCam as Camera3D
+@onready var _camera_pivot := $CameraPivot as Node3D
+@onready var _camera_yaw := self;
+@export_range(0,360) var gamepad_sensitivity = 180;
+@export_range(0.0, 1.0) var mouse_sensitivity = 0.01
+@export var tilt_limit = deg_to_rad(75)
+@export var target:Node3D;
+static var singleton:Player_Cam=null;
 # Called when the node enters the scene tree for the first time.
 func _ready():
+	singleton=self;
+	if (get_parent()):
+		call_deferred("reparent",get_tree().current_scene);
 	pass # Replace with function body.
-
-
+func _unhandled_input(event: InputEvent) -> void:
+	if !_camera_pivot or !_camera_yaw:
+		return;
+	if event is InputEventMouseMotion:
+		_camera_yaw.rotation.y -= event.relative.x * mouse_sensitivity
+		# Prevent the camera from rotating too far up or down.
+		_camera_pivot.rotation.x += -event.relative.y * mouse_sensitivity
+		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
 # Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta):
+func _physics_process(delta):
+	if (target):
+		global_position=target.global_position;
+	var cam_dir=Input.get_vector("ui_cam_left","ui_cam_right","ui_cam_up","ui_cam_down");
+	if cam_dir:
+		_camera_yaw.rotation.y -= deg_to_rad(cam_dir.x*gamepad_sensitivity*delta);
+		# Prevent the camera from rotating too far up or down.
+		_camera_pivot.rotation.x += deg_to_rad(cam_dir.y*gamepad_sensitivity*delta);
+		_camera_pivot.rotation.x = clampf(_camera_pivot.rotation.x, -tilt_limit, tilt_limit)
 	pass
